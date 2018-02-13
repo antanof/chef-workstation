b4096d971434a2982b23a8050429b48cbc0b1693
vagrant <vagrant@localhost.localdomain>
2018-02-13 14:05:51 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,21 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntp'
+package 'vim-enhanced'
+package 'nano'
+package 'git' do
+  action :install
+end
+
+service 'ntpd' do
+  action [ :start, :enable ]
+end
+
+file '/etc/motd' do
+  content 'This server is the property of antanof'
+  owner 'root'
+  group 'root'
+  action :create
+end
