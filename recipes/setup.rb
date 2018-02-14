0fba1d48c3914593caa6944dbcff007e33adc4c3
antanof <matthieu.fontana@itcloud.ovh>
2018-02-14 09:23:45 +0000

    addded ohai gather facts

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -26,12 +26,16 @@ service 'ntpd' do
   action [ :start, :enable ]
 end
 
+#node['ipaddress']
+#node['memory']['total']
+
 file '/etc/motd' do
-  content 'This server is the property of antanof
-HOSTNAME:
-IPADDRESS:
-CPU:
-MEMORY:'
+  content "This server is the property of antanof
+HOSTNAME: #{node['hostname']}
+IPADDRESS: #{node['ipaddress']}
+CPU: #{node['cpu']['0']['mhz']}
+MEMORY: #{node['memory']['total']}
+"
   owner 'root'
   group 'root'
   action :create
