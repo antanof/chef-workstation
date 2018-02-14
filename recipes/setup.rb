9b5ff13ebe141f7bff2264a300e27a8c1df4991d
antanof <matthieu.fontana@itcloud.ovh>
2018-02-14 11:02:32 +0000

    refactored motd

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -29,13 +29,8 @@ end
 #node['ipaddress']
 #node['memory']['total']
 
-file '/etc/motd' do
-  content "This server is the property of antanof
-HOSTNAME: #{node['hostname']}
-IPADDRESS: #{node['ipaddress']}
-CPU: #{node['cpu']['0']['mhz']}
-MEMORY: #{node['memory']['total']}
-"
+template '/etc/motd' do
+  source 'motd.erb'
   owner 'root'
   group 'root'
   action :create
